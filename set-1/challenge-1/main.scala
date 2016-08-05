24f3f772cf16b4fef05724885a58eef217ea53f6
DomDuxbury <domduxbury@hotmail.com>
2016-08-05 12:03:25 +0100

    Completed and refactored first challenge

--- set-1/challenge-1/main.scala
+++ set-1/challenge-1/main.scala
@@ -1,10 +1,16 @@
 object app {
   def main(args: Array[String]): Unit = {
     try {
-      val base16 = args(0)
-      val value = Integer.parseInt(base16, 16)
-      val base64 = value.toHexString(64)
-      println(base64)
+      val input = args(0)
+      val encoder = java.util.Base64.getEncoder()
+
+      // Convert input from base 16 to Bytes
+      val bigInt = new java.math.BigInteger(input, 16).toByteArray()
+
+      // encode bytes as base64 str
+      val ans = encoder.encodeToString(bigInt);
+
+      println(ans)
     } catch {
       case e : Exception => println(e)
     }
